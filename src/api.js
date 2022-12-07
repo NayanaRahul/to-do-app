28e8db4cb52d5dc2814da177081df3bd6948241f
Nayana Rahul <nayanasubhash22@gmail.com>
2022-12-07 12:49:19 +0000

    Add api to delete a task

--- src/api.js
+++ src/api.js
@@ -15,7 +15,7 @@ const getList = () => {
 /* Return all tasks based on search text */
 const seachTask = (searchText) => {
   return axios
-    .post("http://localhost:3001/searchtask", searchText)
+    .post("http://localhost:3001/searchtask", { searchText })
     .then((response) => {
       return response.data;
     })
@@ -25,7 +25,7 @@ const seachTask = (searchText) => {
 };
 
 /* Return Active Tasks count */
-const getActiveTaskCount = (searchText) => {
+const getActiveTaskCount = () => {
   return axios
     .get("http://localhost:3001/activeTaskCount")
     .then((response) => {
@@ -36,4 +36,16 @@ const getActiveTaskCount = (searchText) => {
     });
 };
 
-export { getList, seachTask, getActiveTaskCount };
+/* Delete a task using task id */
+const deleteTask = (taskId) => {
+  return axios
+    .post("http://localhost:3001/deleteTask", { taskId })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { getList, seachTask, getActiveTaskCount, deleteTask };
