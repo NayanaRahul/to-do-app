7083d3075c8a7e16bd8f1f6b65440e1b4dc16f66
Nayana Rahul <nayanasubhash22@gmail.com>
2022-12-22 14:09:30 +0000

    Add api to update a task

--- src/api.js
+++ src/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 /* Return all the tasks available */
-const getList = () => {
-  return axios
-    .get("http://localhost:3001/gettasks")
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const getList = async () => {
+  console.log("GET LIST");
+  try {
+    const response = await axios.get("http://localhost:3001/gettasks");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 /* Return all tasks based on search text */
@@ -37,15 +36,28 @@ const getActiveTaskCount = () => {
 };
 
 /* Delete a task using task id */
-const deleteTask = (taskId) => {
-  return axios
-    .post("http://localhost:3001/deleteTask", { taskId })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      console.log(err);
+const deleteTask = async (taskId) => {
+  try {
+    const response = await axios.post("http://localhost:3001/deleteTask", {
+      taskId,
     });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export { getList, seachTask, getActiveTaskCount, deleteTask };
+/* Update a task */
+const updateTask = async (task, taskId) => {
+  try {
+    const response = await axios.post("http://localhost:3001/updateTask", {
+      task,
+      taskId,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { getList, seachTask, getActiveTaskCount, deleteTask, updateTask };
